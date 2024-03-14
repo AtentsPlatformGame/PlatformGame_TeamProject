e4342098f7c07bee1307a55b728c8be1c6aec642
KOSUNGHOON <dlrjsgml39@gmail.com>
2024-03-14 13:48:26 +0900

    상점 UI
    11111

--- PlatformGameTP/Assets/Scripts/UI_Shop/Shop.cs
+++ PlatformGameTP/Assets/Scripts/UI_Shop/Shop.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start()=> this.gameObject.SetActive(false);
 
     // Update is called once per frame
     void Update()
