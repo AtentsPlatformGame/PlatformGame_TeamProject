0e4916bac1ab3c9bc45c1edf148196b780240a4f
rnfmazkdlfks <tonysay1117@gmail.com>
2024-03-15 12:46:46 +0900

    UI_Options

--- PlatformGameTP/Assets/Scripts/UI_Inventory/Inventory.cs
+++ PlatformGameTP/Assets/Scripts/UI_Inventory/Inventory.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] Transform MyInventory;
+    [SerializeField] Transform MyOptions;
+    public bool checkInventory;
+    public bool checkOptions;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (MyInventory != null)
+        {
+            checkInventory = false;
+            MyInventory.gameObject.SetActive(false);
+            checkOptions = false;
+            MyOptions.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (checkInventory == false && checkOptions == false)
+            {
+                PopUp(MyInventory);
+                checkInventory= true;
+            }
+            else
+            {
+                PopDown(MyInventory);
+                checkInventory= false;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (checkOptions == false && checkOptions == false)
+            {
+                PopUp(MyOptions);
+                checkOptions= true;
+            }
+            else
+            {
+                PopDown(MyOptions);
+                checkOptions= false;
+            }
+        }
+
+
     }
+
+    public void PopUp(Transform popup)
+    {
+        popup.gameObject.SetActive(true);
+    }
+    public void PopDown(Transform popup)
+    {
+        popup.gameObject.SetActive(false);
+    }
+
 }
+
