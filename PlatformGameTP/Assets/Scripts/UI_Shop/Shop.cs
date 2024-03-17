d7b2e86302fdda7380e0291c048e3b26f7dfabfc
awesome1235 <dlrjsgml39@gmail.com>
2024-03-18 02:14:53 +0900

    상점 스크립트
    어렵다

--- PlatformGameTP/Assets/Scripts/UI_Shop/Shop.cs
+++ PlatformGameTP/Assets/Scripts/UI_Shop/Shop.cs
@@ -10,6 +10,34 @@ public class Shop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        using UnityEngine;
+        using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+    {
+
+        public void SetItem(ShopItem item)
+        {
+            currentItem = item;
+            itemNameText.text = item.itemName;
+            itemPriceText.text = "Price: " + item.price.ToString();
+        }
+
+        public void BuyItem()
+        {
+            if (currentItem != null && playerInventory != null)
+            {
+                if (playerInventory.BuyItem(currentItem))
+                {
+                    Debug.Log("Item purchased: " + currentItem.itemName);
+                   
+                }
+                else
+                {
+                    Debug.Log("Not enough money to buy: " + currentItem.itemName);
+                   
+                }
+            }
+        }
     }
 }
