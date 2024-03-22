89e29e91f91622da62f395fcbe168e4d8c668280
namho395 <namho395@gmail.com>
2024-03-22 13:42:48 +0900

    카메라 시점에 따른 이동과 스펠 사용 수정

--- PlatformGameTP/Assets/Scripts/Character/Player/CameraController.cs
+++ PlatformGameTP/Assets/Scripts/Character/Player/CameraController.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Events;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform[] cameraPos;
     [SerializeField] Transform orgPos;
     [SerializeField] CinemachineVirtualCamera playerCam;
+
+    public UnityEvent<bool> changePlayerControllType;
+
     float keyDownTime = 0.0f;
     bool isCamera3D = false;
+    Vector3 originCamRot;
+    Coroutine rotatingC;
+    Vector3 targetRot;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +26,12 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isCamera3D)SetCamera3D();
-        MoveCameraFocus();
+        if (!isCamera3D)
+        {
+            MoveCameraFocus();
+        }
+
+        
     }
 
     void MoveCameraFocus()
@@ -68,35 +79,62 @@ public class CameraController : MonoBehaviour
     // 플레이모드가 2D일 때 카메라 위치 변경
     public void SetCamera2D()
     {
+        isCamera3D = false;
+        originCamRot = playerCam.transform.localEulerAngles;
         // localRoation x = 0, y = 270, z = 0;
-        Vector3 rot2D = new Vector3(0.0f, 270.0f, 0.0f);
-        playerCam.transform.localEulerAngles = rot2D;
+        targetRot = new Vector3(0.0f, 270.0f, 0.0f);
+        /*if(rotatingC != null)
+        {
+            StopCoroutine(rotatingC);
+        }*/
+        //rotatingC = StartCoroutine(RotatingCam(targetRot2D));
+        playerCam.transform.localEulerAngles = targetRot;
+        changePlayerControllType?.Invoke(true);
 
     }
 
     // 플레이 모드가 3D 일 때 카메라 위치 변경 >> 여긴 추후 개발 예정, 마우스의 움직임에 따라 카메라가 일정 거리에서 따라오게 만들거임
     public void SetCamera3D() // 여기선 Lerp
     {
+        isCamera3D = true;
         // localRoation x = 30, y = 300, z = 0;
-        Vector3 originCamRot = playerCam.transform.localEulerAngles;
-        Vector3 rot3D = new Vector3(30.0f, 300.0f, 0.0f);
+        originCamRot = playerCam.transform.localEulerAngles;
+        targetRot = new Vector3(60.0f, 0.0f, 0.0f);
 
-        Vector3 targetRot = Vector3.Lerp(originCamRot, rot3D, Time.deltaTime * 10.0f);
-
-        playerCam.transform.localEulerAngles = rot3D;
+        /*if (rotatingC != null)
+        {
+            StopCoroutine(rotatingC);
+        }
+        rotatingC = StartCoroutine(RotatingCam(targetRot3D));*/
+        /*Vector3 targetRot = Vector3.Lerp(originCamRot, targetRot3D, Time.deltaTime * 10.0f);
+        */
+        playerCam.transform.localEulerAngles = targetRot;
+        changePlayerControllType?.Invoke(false);
     }
 
     // 플레이모드가 탑 뷰일 때 카메라 위치 변경
     public void SetCameraTopView()
     {
+        isCamera3D = true;
+        /*if (rotatingC != null)
+        {
+            StopCoroutine(rotatingC);
+        }*/
         // localRoation x = 90, y = 0, z = 0;
-        Vector3 rotTopView = new Vector3(90.0f, 0.0f, 0.0f);
-        playerCam.transform.localEulerAngles = rotTopView;
-
+        targetRot = new Vector3(90.0f, 0.0f, 0.0f);
+        playerCam.transform.localEulerAngles = targetRot;
+        changePlayerControllType?.Invoke(false);
     }
 
-    public void Camera3DToggle(bool _is3D)
+    IEnumerator RotatingCam(Vector3 targetRot)
     {
-        isCamera3D = _is3D;
+        while(targetRot != playerCam.transform.localEulerAngles)
+        {
+            Vector3 tmpRot = Vector3.Lerp(originCamRot, targetRot, Time.deltaTime * 10.0f);
+            playerCam.transform.localEulerAngles = tmpRot;
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+        playerCam.transform.localEulerAngles = targetRot;
     }
 }
