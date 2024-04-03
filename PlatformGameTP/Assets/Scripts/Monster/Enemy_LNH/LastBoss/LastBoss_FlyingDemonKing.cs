a710f3f508060b6649245fe3afa1cb0878c59d17
namho395 <namho395@gmail.com>
2024-04-03 11:21:01 +0900

    스테이지2 씬 배경 키고 마지막 보스 메테리얼 추가

--- PlatformGameTP/Assets/Scripts/Monster/Enemy_LNH/LastBoss/LastBoss_FlyingDemonKing.cs
+++ PlatformGameTP/Assets/Scripts/Monster/Enemy_LNH/LastBoss/LastBoss_FlyingDemonKing.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class LastBoss_FlyingDemonKing : EnemyState
 {
+    public Transform clawAttackEffect; // 할퀴기 공격 vfx
+    public Transform biteAttackEffect; // 깨물기 공격 vfx
+    public Transform fireBallEffect; // 원거리 공격 vfx
+
+    public Transform clawAttackPoint; // 할퀴기 공격 포인트
+    public Transform biteAttackPoint; // 깨물기 공격 포인트
+    public Transform fireBallPoint; // 원거리 공격 발사 포인트
+
+    [SerializeField] Transform spawnMonster1;
+    [SerializeField] Transform spawnMonster2;
+    [SerializeField] bool isPhaseChanged = false;
+
+    protected override void ChangeState(State s)
+    {
+        base.ChangeState(s);
+        switch (myState)
+        {
+            case State.Phase:
+                StopAllCoroutines();
+                myAnim.SetBool("IsRoaming", false);
+                myAnim.SetBool("IsRunning", false);
+                myAnim.SetTrigger("Spawn");
+                //StartCoroutine(SpawnSkeleton(spawnPoint));
+                break;
+            default:
+                break;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
