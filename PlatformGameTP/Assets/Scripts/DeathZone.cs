c1aa09fa0d63908e90ea06456d8498b3c4a8dcc0
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2024-05-04 16:32:36 +0900

    낙사 수정

--- PlatformGameTP/Assets/Scripts/DeathZone.cs
+++ PlatformGameTP/Assets/Scripts/DeathZone.cs
@@ -7,6 +7,7 @@ public class DeathZone : MonoBehaviour
 {
     public Transform player;
     public Transform playerCam;
+    public Transform StartPos;
     public LayerMask playerMask;
 
     private void OnTriggerEnter(Collider other)
@@ -16,6 +17,10 @@ public class DeathZone : MonoBehaviour
             BattleSystem bs = player.GetComponent<BattleSystem>();
             if(bs != null)
             {
+                if (player.GetComponent<PlayerController>().GetResurrectionOneTime())
+                {
+                    if(StartPos != null) player.transform.position = StartPos.transform.position;
+                }
                 bs.TakeDamage(100f);
                 playerCam.GetComponent<CinemachineVirtualCamera>().Follow = null;
             }
